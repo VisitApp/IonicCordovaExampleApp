a8f1e2369f8bae286500013de8d934630b332eec
chinmay <chinmaydash11@gmail.com>
2023-08-16 19:28:18 +0530

    added updated plugin

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Camera, CameraOptions } from '@awesome-cordova-plugins/camera/ngx';
 
 
+
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -44,6 +46,32 @@ export class HomePage {
       alert(err)
     });
   }
+
+  addNumber(){
+      (<any>window).cordova.plugins.CordavaFitnessPlugin.coolMethod([2,3],(data:any) => {
+          console.log(data);
+      });
+
+  }
+
+  openVisitApp(){
+    (<any>window).cordova.plugins.CordovaFitnessPlugin.open(
+      [
+        "https://itgi.getvisitapp.xyz/sso?userParams=kdRDkaQXGejyMZrFaX%2FMraWZoXzqXQr3u6HEsC%2BMuX4OOZSjH%2BKOYbC9NKcQTKVkboq2QhRpDLq4LxhpJC%2Frr4%2Bo5rQojgwC7JzKIR4MJr6mrF%2BKxwSm2jG7HrPRod7Z%2Bya8DCcyz%2F18aMfFxX7bWfH7uXKds3E%2FExtEMcZAJhKqIJ%2FUypD0zPbRn1IlgHJIqXDBYYOLb5pHe1Pn85LQ2TOEj8UE5zrJzNIxbZFs0e8xVarmQYnKrDUWH%2BmaZ4dQr1hLzhqk0svtJhJifvzvjdwWisgQz8QJ%2Bv7hAmALQdZhWKlu%2FWAwz1XS7peecPc4E9T8y2ZwwhQJeBoZ4YS7dI4xcigz6U8t&clientId=itgi-sdk-012",
+       "967914547335-g2ntga70t1i7b19ti91gcubb7agm7rje.apps.googleusercontent.com",
+    ]);
+     
+    
+}
+
+ openNewActivity(){
+
+  (<any>window).cordova.plugins.CordavaFitnessPlugin.new_activity([],(data:any) => {
+    console.log(data);
+ });
+          
+        
+}
 
 
 }
